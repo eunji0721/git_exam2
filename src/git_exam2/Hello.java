f57a2fff618bf2874bf5ad47bcc97bce285b5541
eunji0721 <ghddmswl721@gmail.com>
2021-10-26 09:35:16 +0900

    201914075-3

--- src/git_exam2/Hello.java
+++ src/git_exam2/Hello.java
@@ -3,6 +3,7 @@ package git_exam2;
 public class Hello {
 
 	public static void main(String[] args) {
+		System.out.println("IT융합자율학부");
 		System.out.println("201914075");
 		System.out.println("홍은지");
 	}
